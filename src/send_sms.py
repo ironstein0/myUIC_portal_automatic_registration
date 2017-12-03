cb89aa718f9bf845eeb067efe67b5746a7fbc1e1
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-12-03 06:20:48 -0600

    modified config

--- src/send_sms.py
+++ src/send_sms.py
@@ -1,38 +1,19 @@
 from twilio.rest import Client
+from config import credentials
 
-# Your Account SID from twilio.com/console
-account_sid = "ACf517d947feec9a53125b26ae9c9aad17"
-# Your Auth Token from twilio.com/console
-auth_token  = "654b649c1999a5e9b4dbe2c94687a7d6"
-# Twilio Phone Number
-twilio_number = '+14243040690'
-# My phone numbe
+account_sid = credentials.ACCOUNT_SID
+auth_token = credentials.AUTH_TOKEN
+twilio_number = credentials.TWILIO_NUMBER
+phone_numbers = credentials.USER_PHONE_NUMBER
 
 def send_sms(message_body):
     client = Client(account_sid, auth_token)
 
-    message = client.messages.create(
-        to=     "+18723339826", 
-        from_=  twilio_number,
-        body=   message_body
-    )
+    for phone_number in phone_numbers:
+        message = client.messages.create(
+            to=     phone_number, 
+            from_=  twilio_number,
+            body=   message_body
+        )
 
-    print(message.sid)
-
-    client = Client(account_sid, auth_token)
-
-    message = client.messages.create(
-        to=     "+13127926471", 
-        from_=  twilio_number,
-        body=   message_body
-    )
-
-    print(message.sid)
-
-    # message = client.messages.create(
-    #     to=     "+13128387836", 
-    #     from_=  "+16307915714",
-    #     body=   message_body
-    # )
-
-    # print(message.sid)
+        print(message.sid)
